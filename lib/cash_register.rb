9b9dd51cd962c87f942d6b9790c9669627023694
Snicewicz <snicewicz@gmail.com>
2022-01-25 15:21:33 -0500

    commit tree 7dd1a47fc846ebb3ce1e6fdb70be3502031d19b7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    attr_accessor :total
+
+    def initialization(total = 0.0, discount = 0)
+        @total = total
+        if discount != 0
+        @discount = discount
+        end
+    end
+
+    def total
+        @total 
+    end
+
+
+    def add_item(title, price)
+    end
+
+    def apply_discount(discount)
+    end
+
+    def items
+    end
+
+    def void_last_transaction(item)
+    end
+
+end
+
+
 
